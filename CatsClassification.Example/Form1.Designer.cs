a6ddc8c589ca86d57572745f16b17c2c560068ae
Yuri Vetroff <yuri.vetroff@gmail.com>
2018-11-20 17:29:40 +0300

    Add draft for cats classification on GUI

--- CatsClassification.Example/Form1.Designer.cs
+++ CatsClassification.Example/Form1.Designer.cs
@@ -28,23 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClassify = new System.Windows.Forms.Button();
+            this.groupBoxResult = new System.Windows.Forms.GroupBox();
+            this.labelClassCaption = new System.Windows.Forms.Label();
+            this.labelClass = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.groupBoxResult.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(258, 258);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMain.TabIndex = 0;
+            this.pictureBoxMain.TabStop = false;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpen.Location = new System.Drawing.Point(276, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(149, 35);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonClassify
+            // 
+            this.buttonClassify.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClassify.Location = new System.Drawing.Point(276, 53);
+            this.buttonClassify.Name = "buttonClassify";
+            this.buttonClassify.Size = new System.Drawing.Size(149, 35);
+            this.buttonClassify.TabIndex = 2;
+            this.buttonClassify.Text = "Classify";
+            this.buttonClassify.UseVisualStyleBackColor = true;
+            this.buttonClassify.Click += new System.EventHandler(this.buttonClassify_Click);
+            // 
+            // groupBoxResult
+            // 
+            this.groupBoxResult.Controls.Add(this.labelClass);
+            this.groupBoxResult.Controls.Add(this.labelClassCaption);
+            this.groupBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxResult.Location = new System.Drawing.Point(276, 94);
+            this.groupBoxResult.Name = "groupBoxResult";
+            this.groupBoxResult.Size = new System.Drawing.Size(149, 51);
+            this.groupBoxResult.TabIndex = 4;
+            this.groupBoxResult.TabStop = false;
+            this.groupBoxResult.Text = "Result";
+            // 
+            // labelClassCaption
+            // 
+            this.labelClassCaption.AutoSize = true;
+            this.labelClassCaption.Location = new System.Drawing.Point(6, 22);
+            this.labelClassCaption.Name = "labelClassCaption";
+            this.labelClassCaption.Size = new System.Drawing.Size(52, 20);
+            this.labelClassCaption.TabIndex = 0;
+            this.labelClassCaption.Text = "Class:";
+            // 
+            // labelClass
+            // 
+            this.labelClass.AutoSize = true;
+            this.labelClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClass.Location = new System.Drawing.Point(59, 22);
+            this.labelClass.Name = "labelClass";
+            this.labelClass.Size = new System.Drawing.Size(49, 20);
+            this.labelClass.TabIndex = 2;
+            this.labelClass.Text = "none";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(437, 282);
+            this.Controls.Add(this.groupBoxResult);
+            this.Controls.Add(this.buttonClassify);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.pictureBoxMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cats classifier";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.groupBoxResult.ResumeLayout(false);
+            this.groupBoxResult.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonClassify;
+        private System.Windows.Forms.GroupBox groupBoxResult;
+        private System.Windows.Forms.Label labelClassCaption;
+        private System.Windows.Forms.Label labelClass;
     }
 }
 
--- CatsClassification.Example/Form1.cs
+++ CatsClassification.Example/Form1.cs
@@ -1,20 +1,62 @@
-﻿using System;
+﻿using CatsClassification.Common;
+using CatsClassification.Running;
+using CNTK;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CatsClassification.Example
 {
     public partial class Form1 : Form
     {
+        private const string TRAINED_MODEL_FILE = "cats-classification/cats-classifier-trained.model";
+        private readonly CntkModelWrapper _modelWrapper;
+        private readonly DeviceDescriptor _device = DeviceDescriptor.CPUDevice;
+        private string _imageFile = "";
+        private readonly IReadOnlyDictionary<int, string> _outputToClassMap = new Dictionary<int, string>()
+        {
+            { 0, "Tiger" },
+            { 1, "Leopard" },
+            { 2, "Puma" }
+        };
+
         public Form1()
         {
             InitializeComponent();
+            _modelWrapper = new CntkModelWrapper(TRAINED_MODEL_FILE, _device);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Title = "Open Image";
+                dialog.Filter = "jpg files (*.jpg)|*.jpg";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    _imageFile = dialog.FileName;
+                    pictureBoxMain.Image = new Bitmap(dialog.FileName);
+                }
+            }
+        }
+
+        private void buttonClassify_Click(object sender, EventArgs e)
+        {
+            var inputValue = new Value(new NDArrayView(new int[] { 224, 224, 3 }, ImageHelper.Load(224, 224, _imageFile), _device));
+            var inputDataMap = new Dictionary<Variable, Value>() { { _modelWrapper.Input, inputValue } };
+            var outputDataMap = new Dictionary<Variable, Value>() { { _modelWrapper.EvaluationOutput, null } };
+
+            _modelWrapper.Model.Evaluate(inputDataMap, outputDataMap, _device);
+            var outputData = outputDataMap[_modelWrapper.EvaluationOutput].GetDenseData<float>(_modelWrapper.EvaluationOutput).First();
+
+            var output = outputData.Select(x => (double)x).ToArray();
+            var classIndex = Array.IndexOf(output, output.Max());
+            var className = _outputToClassMap[classIndex];
+
+            labelClass.Text = className;
         }
     }
 }
